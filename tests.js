02bb8846b8067afe0fa7ae5716c5f87091933dae
0r4nd <103434548+0r4nd@users.noreply.github.com>
2023-04-01 17:25:41 +0200

    less buggy

--- tests.js
+++ tests.js
@@ -1,41 +1,24 @@
       
-'use strict';
-
-var cumul = '';
-
-//function check(input, obj, comment="") {
-//  var r = of(obj);
-//  cumul += "| `of("+input+")` | " + "`{type:'"+r.type+"', super:'" + r.super + "'}` | " + comment + " |\n";
-//}
 
 function tests() {
   console.clear();
-  
-  //console.log(BBCode.html("hey[b]ça va ?[/b]"));
-  /*
-  // improved console.log()
-  BBCode.log("[b]bbcode[/b][font=Impact]hello [/font][color=red]world[/color] of [b]bbcode[/b]");
-  BBCode.log("this is a [b]bold [u]underlined and [color=blue]colored[/color][/u][/b] [size=125%]big text[/size]");
+  document.body.innerHTML += 'BBCODE to HTML TESTS:<br><br>';
 
-  // bbcode to html
-  console.log(BBCode.html("[font=Impact]hello [/font][color=red]world[/color]"));
-  console.log(BBCode.html("this is a [b]bold [u]underlined and [color=blue]colored[/color][/u][/b] [size=125%]big text[/size]"));
+  var s = "- this is a [b]bold [u]underlined, [color=blue]blue[/color] and [color=red]red[/color][/u][/b] [size=200%]big text[/size]";
+  var sres = BBCode.html(s);
+  BBCode.log(s);
+  document.body.innerHTML += sres;
+  console.log(sres)
 
-  document.body.innerHTML = "Open console for see console tests (ctrl+k)<br>";
-  */
+  document.body.innerHTML += '<br>';
 
-  //var s = "[b]salut[b]salut2[/b][/b]";
-  var s = "[size=150%]salut[b]salut2[/b][/size]";
-
-  var html = BBCode.html(s);
-  document.body.innerHTML += html;
-  console.log("\n")
-  console.warn("original string:", s)
-  console.log(html)
-  //document.body.innerHTML += BBCode.html("[b]bbcode[/b] hello [s]bbcode[/s]");
+  var s = "- [font=Impact]this is 'Impact' font [/font][color=rgb(255,0,0)] red [i]italic text[/i][/color] ";
+  var sres = BBCode.html(s);
+  BBCode.log(s);
+  document.body.innerHTML += sres;
+  console.log(sres)
+  console.log(document.body.innerHTML)
 }
-
-
 
 
 
